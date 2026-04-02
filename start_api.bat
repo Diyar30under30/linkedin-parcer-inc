5ad950baa0c2c2a5f50f240c4af5a4775b0725fb
Diyar30under30 <diaramanzolov@gmail.com>
2026-04-02 09:47:32 +0500

    code

--- start_api.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call venv\Scripts\activate.bat
-echo ===========================================
-echo LinkedIn Parser API Server
-echo ===========================================
-echo.
-python fastapi_server.py
-pause
